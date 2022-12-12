5d4a9300341eb308eab4fbb4b35e0ae031491b8b
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-12-12 09:28:18 -0300

    Insert

--- app.js
+++ app.js
@@ -2,19 +2,11 @@
 
 
 
-const namesGame = [
-    'Pedra',
-    'Papel',
-    'Tesoura'
-];
+const namesGame = ['Pedra','Papel','Tesoura'];
 const AIPlayer = [1,2,3];
-const resFeedback = [
-    '13',
-    '21',
-    '32',
-];
+const resFeedback = ['1-0','0-2','2-1',];
 let pointsYou = 0;
-let pointsAI;
+let pointsAI = 0;
 const gameFunction = {
     select: function(token) {
         let gameAI = this.randonAI();
@@ -26,16 +18,29 @@ const gameFunction = {
         return roundAI;
     },
     gameCheck: function(token1, token2) {
-        let resUnif = `${token1 - 1}${token2}`;
+        let resUnif = `${token1 - 1}-${token2}`;
+        document.querySelector('.containerChoice').style.display = 'none';
+        setTimeout(() => {
+            document.querySelector('.containerChoice').style.display = 'block';
+            document.querySelector('#gameYou').innerHTML = `Esperando...`;
+            document.querySelector('#gameAI').innerHTML = `Esperando...`;
+        },1500);
         resFeedback.forEach(res => {
-            if(res === resUnif) {
+            if(res == resUnif) {
                 pointsYou ++;
-                document.querySelector('#points').innerHTML = pointsYou; 
+                document.querySelector('#points').innerHTML = `Points (you) = ${pointsYou}`;
+            } else {
+                if(token1 - 1 == token2) {
+                    document.querySelector('.empateTitle').style.opacity = '1';
+                    setTimeout(() => {
+                        document.querySelector('.empateTitle').style.opacity = '0';
+                    }, 1400);
+                };
             };
         });
     },
     titleRound: function(token1, token2) {
-        document.querySelector('#gameAI').innerHTML = `Player2 = ${namesGame[token2]}`;
-        document.querySelector('#gameYou').innerHTML = `Player 1 (you) = ${namesGame[token1 - 1]}`;
+        document.querySelector('#gameYou').innerHTML = `${namesGame[token1 - 1]}`;
+        document.querySelector('#gameAI').innerHTML = `${namesGame[token2]}`;
     },
 };
